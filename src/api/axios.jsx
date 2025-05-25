91477f6841cafd7719fc032903c45733eb6547ef
Sachin Bhadoriya <bhadoriyasachin33@gmail.com>
2025-05-25 13:47:23 +0530

    wrk

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://hill-view-backend-1.onrender.com",
+  baseURL: "https://hill-view-backend-1.onrender.com/api",
 });
 
 export default api;
